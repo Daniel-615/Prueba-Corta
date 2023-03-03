671546f0413ac39555594dfe31b6f101af1c9d29
Daniel-615 <suyanangel615@gmail.com>
2023-03-02 19:34:15 -0600

    Primer commit

--- Persona.cpp
+++ Persona.cpp
@@ -1,67 +1,22 @@
 #include <iostream>
-#include "Propietario.cpp"
+
 using namespace std;
-class Persona: Propietario{
-	private: string nombres,apellidos,direccion,fecha_nacimiento;
-	int telefono;
-	public: 
-	Persona(){
-	}
-	Persona(string n, double c,string nom,string ape,string dir,string date,int tel) : Propietario(n,c){
-		nombres=nom;
-		apellidos=ape;
-		direccion=dir;
-		fecha_nacimiento=date;
-		telefono=tel;
-	}
-	//Metodo agregar
-	void agregar(){
-		cout<<"Se han agregado los atributos correctamente\n";
-	}
-	//Metodo Set
-	void setNit(string ni){
-		nit=ni;
-	}
-	void setCui(double cu){
-		cui=cu;
-	}
-	void setNombre(string n){
-		nombres=n;
-	}
-	void setApellidos(string ap){
-		apellidos=ap;
-	}
-	void setDireccion(string dire){
-		direccion=dire;
-	}
-	void setDate(string da){
-		fecha_nacimiento=da;
-	}
-	void setTel(int tel){
-		telefono=tel;
-	}
-	//Metodo get
-	 string getNit(){
-        return nit;
-	}
-    double getcui(){
-    	return cui;
-    }
-	string getNombres(){
-        return nombres;
-    }
-    string getApe(){
-    	return apellidos;
-    }
-	string getdir(){
-   		return direccion;
-    }
-    int getTel(){
-		return telefono;
-    }
-    //mostrar
-    void mostrar(){
-    cout<<"==============="<<endl;
-    cout<<nit<<","<<cui<<","<<nombres<<","<<apellidos<<","<<direccion<<","<<telefono<<","<<fecha_nacimiento<<endl;
-    }
+class Persona {
+protected: string nombres,apellidos,direccion,fecha_nacimiento;
+int telefono;
+  protected:
+    //constructores
+    Persona() {}
+
+  Persona(string nom, string ape, string dir, string fech, int tel) {
+    nombres = nom;
+    apellidos = ape;
+    direccion = dir;
+    telefono = tel;
+    fecha_nacimiento = fech;
+  }
+
+  void agregar();
+  void mostrar();
+  void modificar();
 };
--- main.cpp
+++ main.cpp
@@ -1,42 +1,58 @@
 #include <iostream>
-#include "Persona.cpp"
+#include "Propietario.cpp"
+
 using namespace std;
-int main(){
-	string nit,nombres,apellidos,direccion,fecha_nacimiento;
-    int telefono;
-    double cui;
-    
-	cout<<"Ingrese el nit: ";
-    cin>>nit;
-	cout<<"Ingrese los nombres: ";
-    cin>>nombres;
-    fflush(stdin);
-	cout<<"Ingrese los apellidos: ";
-    cin>>apellidos;
-    cout<<"Ingrese la direccion: ";
-    cin>>direccion;
-    cout<<"Ingrese el telefono: ";
-    fflush(stdin);
-    cin>>telefono;
-    cout<<"Ingrese la fecha de nacimiento: ";
-    cin>>fecha_nacimiento;
-	cout<<"Ingrese el cui: ";
-	cin>>cui;
-	
-	Persona obj=Persona(nit,cui,nombres,apellidos,direccion,fecha_nacimiento,telefono);
-	obj.mostrar();
-	
-//	cout<<"Ingrese el nuevo nit: ";
-//    fflush(stdin);
-//	cin>>nit;
-//	obj.setNit(nit);
-//    cout<<"Ingrese el cui: ";
-//	obj.setCui(cui);
-//	cout<<"Ingrese el nuevo nombre: ";
-//	fflush(stdin);
-//	obj.setNombre(nombres);
-//	cout<<"Ingrese el nuevo apellido: ";
-//	obj.setApellidos(apellidos);
-    obj.agregar();
-	
+
+int main() {
+  string nombres, apellidos, direccion, fecha_nacimiento, nit;
+  int telefono;
+  double cui;
+
+  cout << "Ingresar NIT: ";
+  cin >> nit;
+  cout << "Ingresar CUI: ";
+  cin >> cui;
+  cout << "Ingresar Nombres: ";
+  cin >> nombres;
+  cout << "Ingresar Apellidos: ";
+  cin >> apellidos;
+  cout << "Ingresar Direccion: ";
+  cin >> direccion;
+  cout << "Ingrese Fecha de Nacimiento: ";
+  cin >> fecha_nacimiento;
+  cout << "Ingresar No. Telefono: ";
+  cin >> telefono; 
+  //Instancias
+  int opc;
+  Propietario obj = Propietario(nombres, apellidos, direccion, fecha_nacimiento, telefono, nit, cui);
+obj.mostrar();	
+obj.agregar();
+	cout << "ingrese nuevo NIT: ";
+    cin >> nit;
+    obj.setNit(nit);
+    obj.mostrar();
+    cout << "ingrese nuevo CUI: ";
+    cin >> cui;
+    obj.setCui(cui);
+    obj.mostrar();
+    cout << "ingrese nuevos Nombres: ";
+    cin >> nombres;
+    obj.setNombre(nombres);
+    obj.mostrar();
+    cout << "ingrese nuevo Apellidos: ";
+    cin >> apellidos;
+    obj.setApellido(apellidos);
+    obj.mostrar();
+    cout << "ingrese nueva Direccion: ";
+	cin >> direccion;
+	obj.setDireccion(direccion);
+    obj.mostrar();
+    cout << "ingrese nueva Fecha de nacimiento: ";
+    cin >> fecha_nacimiento;
+    obj.setFecha(fecha_nacimiento);
+    obj.mostrar();
+	cout << "ingrese nuevo Telefono: ";
+    cin >> telefono;
+    obj.setTelefono(telefono);
+    obj.mostrar();
 }
